d5585b8ee32e6085034d5e4d8fc7870426d8dc52
SiamAnzir <siamanzir18@gmail.com>
2022-06-07 16:57:29 +0600

    [Added] mergeSort algorithm

---
+++ src/SortingAlgorithms/mergeSort.js
@@ -0,0 +1,17 @@
+const getMergeSortAnimations = (array) => {
+    const animations = [];
+    if (array.length <= 1) return array;
+    const auxiliaryArray = array.slice();
+    mergeSortHelper(array, 0, array.length - 1, auxiliaryArray, animations);
+    return animations;
+}
+const mergeSortHelper = (mainArray, startIdx, endIdx, auxiliaryArray, animations) => {
+    if (startIdx === endIdx) return;
+    const middleIdx = Math.floor((startIdx + endIdx) / 2);
+    mergeSortHelper(auxiliaryArray, startIdx, middleIdx, mainArray, animations);
+    mergeSortHelper(auxiliaryArray, middleIdx + 1, endIdx, mainArray, animations);
+    doMerge(mainArray, startIdx, middleIdx, endIdx, auxiliaryArray, animations);
+}
+
+
+export default getMergeSortAnimations;
